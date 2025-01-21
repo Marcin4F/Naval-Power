d4e130985e36d30c947477f79646e7c7556b724f
Marcin4F <markost4f@gmail.com>
2025-01-21 17:12:00 +0100

    tla + rozwiniety tutorial

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -12,7 +12,10 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] Button goBack;
     [SerializeField] Button tutorial;
     [SerializeField] Button tutorialBack;
-    public GameObject mainMenuPanel, optionsPanel, tutorialPanel;
+    [SerializeField] Button nextPage, nextPage2;
+    [SerializeField] Button previousPage, previousPage2;
+    public GameObject mainMenuPanel, optionsPanel, tutorialPanel, tutorialTextPanel1, tutorialTextPanel2, tutorialTextPanel3;
+    private int tutorialPage = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,10 @@ public class MainMenuUI : MonoBehaviour
         goBack.onClick.AddListener(CloseOptions);
         tutorial.onClick.AddListener(OpenTutorial);
         tutorialBack.onClick.AddListener(CloseTutorial);
+        nextPage.onClick.AddListener(NextPage);
+        nextPage2.onClick.AddListener(NextPage);
+        previousPage.onClick.AddListener(PreviousPage);
+        previousPage2.onClick.AddListener(PreviousPage);
         optionsPanel.SetActive(false);
         tutorialPanel.SetActive(false);
     }
@@ -55,11 +62,47 @@ public class MainMenuUI : MonoBehaviour
     {
         mainMenuPanel.SetActive(false);
         tutorialPanel.SetActive(true);
+        tutorialTextPanel2.SetActive(false);
+        tutorialTextPanel3.SetActive(false);
     }
 
     private void CloseTutorial()
     {
+        tutorialPage = 1;
+        tutorialTextPanel1.SetActive(true);
         tutorialPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
+    }
+
+    private void NextPage()
+    {
+        if (tutorialPage == 1)
+        {
+            tutorialTextPanel1.SetActive(false);
+            tutorialTextPanel2.SetActive(true);
+            tutorialPage = 2;
+        }
+        else if (tutorialPage == 2)
+        {
+            tutorialTextPanel2.SetActive(false);
+            tutorialTextPanel3.SetActive(true);
+            tutorialPage = 3;
+        }
+    }
+
+    private void PreviousPage()
+    {
+        if (tutorialPage == 2)
+        {
+            tutorialTextPanel1.SetActive(true);
+            tutorialTextPanel2.SetActive(false);
+            tutorialPage = 1;
+        }
+        else if (tutorialPage == 3)
+        {
+            tutorialTextPanel2.SetActive(true);
+            tutorialTextPanel3.SetActive(false);
+            tutorialPage = 2;
+        }
     }
 }
